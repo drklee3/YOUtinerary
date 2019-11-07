02f868ada0cf0201c53f9b1194e87669120f051d
drklee3 <derrick@dlee.dev>
2019-11-06 20:50:42 -0800

    feat(api): Add koa middleware and basic empty search functions

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,49 @@
-import Koa from 'koa';
-const app = new Koa();
+import Koa        from "koa";
+import Router     from "koa-router";
+import bodyParser from "koa-bodyparser";
 
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
-});
+import {searchLocation, searchRoute} from "./search";
 
-app.listen(3000, () => {
-    console.log("Listening on :3000")
-});
+const app    = new Koa();
+const router = new Router();
+
+const koaInterface = process.env.interface || "127.0.0.1";
+const koaPort      = parseInt(process.env.port || "3000");
+
+async function main() {
+    router.get("/", (ctx) => {
+        ctx.body = "Hello Koa";
+    });
+
+    router.post("/search", async (ctx) => {
+        const { query } = ctx.request.body;
+        ctx.body = searchLocation(query);
+    });
+
+    router.post("/routes", async (ctx) => {
+        const { query } = ctx.request.body;
+        ctx.body = searchRoute(query);
+    })
+
+    app
+        .use(async (ctx, next) => {
+            try {
+                await next();
+            } catch (err) {
+                // will only respond with JSON
+                ctx.status = err.statusCode || err.status || 500;
+                ctx.body = {
+                    message: err.message
+                };
+            }
+        })
+        .use(router.routes())
+        .use(router.allowedMethods())
+        .use(bodyParser());
+
+    app.listen(koaPort, koaInterface, () => {
+        console.log(`Listening on ${koaInterface}:${koaPort}`)
+    });
+}
+
+main();
---
+++ api/src/search.ts
@@ -0,0 +1,23 @@
+export interface SearchResult {
+    address: string;
+    // TODO(Zack): Figure out what fields are required, if there are available
+    // types existing we can use.
+}
+
+export interface Route {
+    // TODO(Slater): Define what route data consists of
+}
+
+export function searchLocation(query: string): SearchResult | undefined {
+    // TODO(Zack): Search for business, location, etc via Google Maps Places API
+    // https://developers.google.com/places/web-service/intro
+    
+    return; // SearchResult
+}
+
+export function searchRoute(query: string): Route | undefined {
+    // TODO(Slater): Search for routes between given locations via Google Maps Routes API
+    // https://developers.google.com/maps/documentation/
+    return;
+}
+
