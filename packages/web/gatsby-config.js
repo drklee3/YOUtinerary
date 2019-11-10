9b57b46fcd48331603d281bfc6f0d200462b14cc
drklee3 <derrick@dlee.dev>
2019-11-10 15:35:53 -0800

    Feat(typescript,web): Add ESLint and allow unused _vars

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -1,35 +1,56 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: "YOUtinerary",
+        description: "Google Calendar and Maps, but worse.",
+        author: "Derrick Lee <derrick@dlee.dev>",
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    `gatsby-plugin-typescript`,
-  ],
-}
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "${__dirname}/src/images",
+            },
+        },
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-plugin-manifest",
+            /* eslint-disable @typescript-eslint/camelcase */
+            options: {
+                name: "gatsby-starter-default",
+                short_name: "starter",
+                start_url: "/",
+                background_color: "#663399",
+                theme_color: "#663399",
+                display: "minimal-ui",
+                icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+            },
+            /* eslint-enable */
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // "gatsby-plugin-offline",
+        {
+            resolve: "gatsby-plugin-typescript",
+            options: {
+                isTSX: true, // defaults to false
+                jsxPragma: "jsx", // defaults to "React"
+                allExtensions: true, // defaults to false
+            },
+        },
+        {
+            resolve: "gatsby-plugin-eslint",
+            options: {
+                test: /\.ts$|\.tsx$/,
+                exclude: /(node_modules|.cache|public)/,
+                stages: ["develop"],
+                options: {
+                    emitWarning: true,
+                    failOnError: false,
+                },
+            },
+        },
+    ],
+};
--- packages/web/src/components/layout.jsx
+++ packages/web/src/components/layout.jsx
@@ -5,53 +5,59 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import Nav from "./Nav"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import { Layout as AntdLayout, Menu } from "antd"
+import React from "react";
+import Nav from "./Nav";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import { Layout as AntdLayout, Menu } from "antd";
 
-import "./layout.css"
+import "./layout.css";
 
-const { Header, Content, Footer } = AntdLayout
+const { Header, Content, Footer } = AntdLayout;
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    const _data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
         }
-      }
-    }
-  `)
+    `);
 
-  return (
-    <>
-      <AntdLayout className="layout">
-        <Header>
-          <Nav />
-        </Header>
-        <Content style={{ padding: "0 50px" }}>
-          <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-            {children}
-          </div>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </AntdLayout>
-    </>
-  )
-}
+    return (
+        <>
+            <AntdLayout className="layout">
+                <Header>
+                    <Nav />
+                </Header>
+                <Content style={{ padding: "0 50px" }}>
+                    <div
+                        style={{
+                            background: "#fff",
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        {children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: "center" }}>
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
+                <footer>
+                    © {new Date().getFullYear()}, Built with
+                    {` `}
+                    <a href="https://www.gatsbyjs.org">Gatsby</a>
+                </footer>
+            </AntdLayout>
+        </>
+    );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
