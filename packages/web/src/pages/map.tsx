3d3c7c4837fb6a86271bc01911033002f95c1932
Pauldin Bebla <sportsman2325@yahoo.com>
2019-11-20 14:23:34 -0800

    updated map page

--- packages/web/src/pages/map.tsx
+++ packages/web/src/pages/map.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "antd/dist/antd.css";
-import SimpleMap from "../components/map";
+import SimpleMap from "../components/map-layout";
 import Layout from "../components/layout";
 
 const MapPage = () => (
