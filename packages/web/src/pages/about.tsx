77973d0cda80376b2375d6fd1a372209a6d2ac79
drklee3 <derrick@dlee.dev>
2019-11-25 14:49:59 -0800

    feat(web): Add basic about page

--- packages/web/src/pages/about.tsx
+++ packages/web/src/pages/about.tsx
@@ -1,34 +1,51 @@
-import { Typography } from "antd";
+import { Col, Row, Typography } from "antd";
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const { Text } = Typography;
+const { Title, Text } = Typography;
 
 const AboutPage = (): JSX.Element => (
     <Layout location="/about">
         <SEO title="About" />
-        <p>yaya</p>
-        <Text strong>
-            <a
-                href="https://github.com/drklee3/YOUtinerary"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                drklee3/YOUtinerary
-            </a>
-        </Text>
-        <br />
-        <Text>
-            Source code licensed{" "}
-            <a
-                href="https://opensource.org/licenses/mit-license.php"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                MIT
-            </a>
-        </Text>
+
+        <Row
+            type="flex"
+            justify="space-around"
+            align="middle"
+            style={{
+                height: "100%",
+                maxWidth: "1200px",
+                margin: "auto",
+                padding: "0 50px 100px",
+            }}
+        >
+            <Col span={24}>
+                <Title level={3}>
+                    <a
+                        href="https://github.com/drklee3/YOUtinerary"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        drklee3/YOUtinerary
+                    </a>
+                </Title>
+                <Title level={4}>
+                    Project for CSCI 187: Design and Management of Software at
+                    SCU.
+                </Title>
+                <Text>
+                    Source code licensed{" "}
+                    <a
+                        href="https://opensource.org/licenses/mit-license.php"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        MIT
+                    </a>
+                </Text>
+            </Col>
+        </Row>
     </Layout>
 );
 
