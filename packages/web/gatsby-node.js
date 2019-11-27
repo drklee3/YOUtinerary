f9200ef470649228bcda78da5a679000b0ab949b
drklee3 <derrick@dlee.dev>
2019-11-27 15:13:00 -0800

    feat(web): Retry GH requests if 202 response

--- packages/web/gatsby-node.js
+++ packages/web/gatsby-node.js
@@ -12,12 +12,29 @@ const crypto = require("crypto");
 const GITHUB_STATS_ENDPOINT =
     "https://api.github.com/repos/drklee3/YOUtinerary/stats/contributors";
 
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function fetchData() {
+    let res;
+    do {
+        res = await axios.get(GITHUB_STATS_ENDPOINT);
+        if (res === 202) {
+            console.log("GitHub responded with a 202, retrying in 1s");
+            await sleep(1000);
+        }
+    } while (res.status === 202);
+
+    return res;
+}
+
 exports.sourceNodes = async ({ actions }) => {
     const { createNode } = actions;
 
     let res;
     try {
-        res = await axios.get(GITHUB_STATS_ENDPOINT);
+        res = await fetchData();
         console.log("Fetched data from GitHub API:", res);
     } catch (e) {
         console.error("Failed to query GitHub API:", e);
